f02da81aca304e80343695a1ead087d10676fa01
Hamid Doosti <hdoosty@gmail.com>
2022-10-27 19:41:47 -0400

    bootstrap reactive project

---
+++ src/test/java/io/doosti/hamid/reactivespring/ReactivespringApplicationTests.java
@@ -0,0 +1,13 @@
+package io.doosti.hamid.reactivespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactivespringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
